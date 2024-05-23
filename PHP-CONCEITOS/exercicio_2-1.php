8bb6db092e7b46d2e2c92ac6d564c187b54bd8e2
Gabriel Santi <gabrielspessoti@gmail.com>
2024-05-23 20:44:43 -0300

    Alterar texto, evitar conflito teste

--- PHP-CONCEITOS/exercicio_2-1.php
+++ PHP-CONCEITOS/exercicio_2-1.php
@@ -1,9 +1,9 @@
 <?php
 
-echo "Digite um número:\n";
+echo "Digite um número: \n";
 $n1 = trim(fgets(STDIN));
 echo "Digite outro número: \n";
 $n2 = trim(fgets(STDIN));
 
 $soma = $n1 + $n2;
-echo "A soma dos números é: $soma";
+echo "A soma dos dois números é: $soma";
